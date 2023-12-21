bee11a8900a2280f80ce292b8295fac203457422
CeilingFan2506 <142992803+CeilingFan2506@users.noreply.github.com>
2023-12-21 11:40:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import streamlit as st
+
+
+
+def get_prediction(data={"sentence":"Hello"}):
+  #copy paste your aiservice link here
+  url = 'https://askai.aiclub.world/5c639127-bc1a-4891-9efa-b83136f034ee'
+  r = requests.post(url, data=json.dumps(data))
+  response = getattr(r,'_content').decode("utf-8")
+  return response
+
+#creating the web app
+#setting the title
+st.title("Car_Quiz")#change the title as your web app
+
+#setting up the sub title
+st.subheader("Guess the car brand")#change the subheader as your web app
+
+#text input
+text = st.text_input("Give the correct answer")
+
+#settin the data
+input_data = {"sentence":text}
+#getting the respoense
+prediction = get_prediction(input_data)
+mood = json.loads(json.loads(prediction)['body'])['predicted_label']
+
+#subheading
+if text:
+  st.subheader("Your Mood is : {}".format(mood))
